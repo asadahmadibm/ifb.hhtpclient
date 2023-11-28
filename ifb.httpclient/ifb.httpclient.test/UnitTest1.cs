eeaa54eeff6a8da886dcd323c3d369f96edf4100
Asad Ahmadi <asad.ahmadi.bm@gmail.com>
2023-11-28 06:57:08 -0800

    test

---
+++ ifb.httpclient/ifb.httpclient.test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace ifb.httpclient.test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
